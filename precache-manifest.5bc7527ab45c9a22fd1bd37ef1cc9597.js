5dec7b102b8a6bfa610762d63e2c738169c6b6b2
balaji030698 <balaji030698@gmail.com>
2021-03-01 23:13:59 +0530

    Updates

---
+++ precache-manifest.5bc7527ab45c9a22fd1bd37ef1cc9597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7d9e0968467d9503389e35db86a80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0b37cd4cf7774fc73b8",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "3df58def3f45dfa6b75b",
+    "url": "/static/js/2.f994d929.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.f994d929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b37cd4cf7774fc73b8",
+    "url": "/static/js/main.073981e6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  }
+]);
